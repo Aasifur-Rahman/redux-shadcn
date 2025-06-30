e6058760589aa1e80e431539996ec311ab04d669
Siam Rahman <asifur.web@gmail.com>
2025-06-30 23:22:29 +0600

    added create modal using shadcn

--- src/pages/Tasks.tsx
+++ src/pages/Tasks.tsx
@@ -1,3 +1,4 @@
+import { AddTaskModal } from "@/components/module/tasks/AddTaskModal";
 import TaskCard from "@/components/module/tasks/TaskCard";
 import { selectFilter, selectTasks } from "@/redux/feature/taskSlice/taskSlice";
 import { useAppSelector } from "@/redux/hook";
@@ -11,8 +12,9 @@ export default function Tasks() {
 
   return (
     <div className="mx-auto max-w-7xl px-5 mt-10">
-      <div>
+      <div className="flex justify-between items-center">
         <h1>Tasks</h1>
+        <AddTaskModal />
       </div>
 
       <div className="space-y-5 mt-5">
